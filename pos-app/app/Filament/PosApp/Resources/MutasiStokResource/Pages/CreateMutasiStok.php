db2f36b39ace3374a3b4c0cc6cb5342efe308b32
rhakagesa <rhakagesa@gmail.com>
2025-07-02 21:51:31 +0700

    menambahkan stok produk dan mngurasi stok bahan baku ketika membuat data mutasi stok

--- pos-app/app/Filament/PosApp/Resources/MutasiStokResource/Pages/CreateMutasiStok.php
+++ pos-app/app/Filament/PosApp/Resources/MutasiStokResource/Pages/CreateMutasiStok.php
@@ -9,4 +9,28 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateMutasiStok extends CreateRecord
 {
     protected static string $resource = MutasiStokResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function afterCreate(): void
+    {
+        $getProdukItem = Produk::find($this->record->produk_id);
+        $getBahanBakuItems = $this->record->bahan_baku;
+        
+        if($getBahanBakuItems && $getProdukItem){
+            foreach($getBahanBakuItems as $bahanBakuItem){
+                $bahanBaku = BahanBaku::find($bahanBakuItem['bahan_baku_id']);
+                $bahanBaku->update([
+                    'stok' => $bahanBaku->stok - $bahanBakuItem['qty']
+                ]);
+            }
+
+            $getProdukItem->update([
+                'stok' => $getProdukItem->stok + $this->record->jumlah_produk
+            ]);
+        }
+    }
 }
