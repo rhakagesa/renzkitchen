7090156556cf0b2dfd4652ee18feeb80fe7f8e03
rhakagesa <rhakagesa@gmail.com>
2025-07-02 21:49:22 +0700

    menambahkan aksesor get dan set untu kebutuhan update, create, view kemudian menerapkan sinkronisasi data agar balance ketika data dihapus dan direstore

--- pos-app/app/Models/MutasiStok.php
+++ pos-app/app/Models/MutasiStok.php
@@ -21,4 +21,52 @@ class MutasiStok extends Model
     {
         return $this->belongsTo(Produk::class);
     }
+
+    public function getBahanBakuAttribute($value)
+    {
+        $value = isset($value) && $value ? \json_decode($value, true) : null;
+        return $value;
+    }
+
+    public function setBahanBakuAttribute($value)
+    {
+        $tempBahanBaku = $value;
+        foreach ($tempBahanBaku as &$item) {
+            unset($item['item_bahan_baku']);
+        }
+        $this->attributes['bahan_baku'] = json_encode($tempBahanBaku);
+    }
+
+    protected static function booted()
+    {
+        static::deleted(function ($mutasiStok) {
+            $bahanBakuList = json_decode($mutasiStok->bahan_baku, true);
+            $getProduk = Produk::find($mutasiStok->produk_id);
+            $jumlahProduksi = $mutasiStok->jumlah_produk;
+            foreach ($bahanBakuList as $item) {
+                $bahanBaku = BahanBaku::find($item['bahan_baku_id']);
+                if ($bahanBaku) {
+                    $bahanBaku->stok += (int) $item['qty'];
+                    $bahanBaku->save();
+                }
+            }
+            $getProduk->stok -= $jumlahProduksi;
+            $getProduk->save();
+        });
+
+        static::restored(function ($mutasiStok) {
+            $bahanBakuList = json_decode($mutasiStok->bahan_baku, true);
+            $getProduk = Produk::find($mutasiStok->produk_id);
+            $jumlahProduksi = $mutasiStok->jumlah_produk;
+            foreach ($bahanBakuList as $item) {
+                $bahanBaku = BahanBaku::find($item['bahan_baku_id']);
+                if ($bahanBaku) {
+                    $bahanBaku->stok -= (int) $item['qty'];
+                    $bahanBaku->save();
+                }
+            }
+            $getProduk->stok += $jumlahProduksi;
+            $getProduk->save();
+        });
+    }
 }
